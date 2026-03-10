9ce76426c6036650afa86f9779d4ba00eba3609e
Anthony C <anthony4lx@hotmail.com>
2026-03-09 21:31:08 -0500

    añadi diseño al login y registro activity

--- kotlin-vista/app/build.gradle.kts
+++ kotlin-vista/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.kotlin_libreria"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.kotlin_libreria"
